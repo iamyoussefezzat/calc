68cb5e6aeb0532210c11555b4744d9e9c5f1cdc8
Youssef M. Ezzat <yz22102004@gmail.com>
2025-10-16 23:50:38 +0300

    fix: correct button spacing and improve event listener variable naming

--- main.js
+++ main.js
@@ -5,26 +5,28 @@ let buttons =document.querySelectorAll
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
+    button.addEventListener('click', (t) =>{
+        if(t.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
         }
 
-        else if(e.target.innerHTML == 'AC'){
+        else if(t.target.innerHTML == 'AC'){
             string = "";
             input.value = string;
         }
         
-        else if(e.target.innerHTML == 'DEL'){
+        else if(t.target.innerHTML == ' DEL'){
             string = string.substring(0, string.length-1);
             input.value = string;
         }
 
        else{
-        string += e.target.innerHTML;
+        string += t.target.innerHTML;
         input.value = string;
        }
         
     }) 
 })
+
+
